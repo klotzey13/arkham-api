11f804d81896e4cafef0641ac31584fe36a1c2e3
Steve Klotz <steve.klotz13@gmail.com>
2025-11-20 22:02:39 -0500

    Add migration to fix some data issues found after trying to see inital data load.

---
+++ Migrations/20251121020020_AddFaction2Card.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arkham.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFaction2Card : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Factions_FactionId",
+                table: "Cards");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Faction2Id",
+                table: "Cards",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_Faction2Id",
+                table: "Cards",
+                column: "Faction2Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Factions_Faction2Id",
+                table: "Cards",
+                column: "Faction2Id",
+                principalTable: "Factions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Factions_FactionId",
+                table: "Cards",
+                column: "FactionId",
+                principalTable: "Factions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Factions_Faction2Id",
+                table: "Cards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Factions_FactionId",
+                table: "Cards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cards_Faction2Id",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "Faction2Id",
+                table: "Cards");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Factions_FactionId",
+                table: "Cards",
+                column: "FactionId",
+                principalTable: "Factions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
